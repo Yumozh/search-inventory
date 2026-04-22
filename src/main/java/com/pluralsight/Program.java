3a0156a0277c87b0f17093c714adf316762c9051
Yumozh <juliiamozhchil@gmail.com>
2026-04-22 14:52:11 -0500

    Print current number of products in the inventory

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,4 +1,31 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Program {
+    public static void main(String[] args) {
+
+        ArrayList<String> inventory = getInventory();
+
+        inventory.add("Ergonomic Mouse");
+
+        System.out.println("Print current number of products: " + inventory.size());
+        System.out.println();
+        for(String product : inventory){
+            System.out.println(product);
+        }
+
+    }
+
+    public static ArrayList<String> getInventory(){
+    ArrayList<String> products = new ArrayList<String>();
+
+        products.add("Laptop (MacBook Pro)");
+        products.add("Mechanical Keyboard");
+        products.add("Ergonomic Office Chair");
+        products.add("27-inch 4K Monitor");
+        products.add("Noise-Canceling Headphones");
+
+        return products;
+    }
 }
